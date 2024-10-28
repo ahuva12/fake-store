febeedb003bc329a1bc60986cfba1bf6b6874b08
ahuva12 <ahuvaeshit@gmail.com>
2024-10-28 12:05:30 +0200

    add expiration time for access to the server of 10 seconds

--- src/service/logic.js
+++ src/service/logic.js
@@ -1,5 +1,5 @@
 
-export const getAllProducts = async () => {
+export const getAllProducts = async (category) => {
     try {
         const res = await fetch('https://fakestoreapi.com/products');
         if (!res.ok) {
@@ -14,13 +14,45 @@ export const getAllProducts = async () => {
 };
 
 export const getRelevantProducts = async (category) => {
-    try {
-        const products = await getAllProducts(); 
-        const relevantProducts = products.filter(prod => prod.category === category); 
-        console.log(relevantProducts);
-        return relevantProducts; // Return the relevant products
-    } catch (error) {
-        console.error('Error fetching relevant products:', error);
-        return []; 
+    
+    if (getTimeDifference(category) >= 10000) {
+        try {
+            const products = await getAllProducts(category); 
+            updateResponseReceivedTime(category);
+            const relevantProducts = products.filter(prod => prod.category === category); 
+            updateProductsInLocalStorage(category, relevantProducts)
+            console.log(relevantProducts);
+            return relevantProducts; // Return the relevant products
+        } catch (error) {
+            console.error('Error fetching relevant products:', error);
+            return []; 
+        }
+    }
+    else {
+        return JSON.parse(localStorage.getItem(`${category} products`));
     }
 };
+
+const updateResponseReceivedTime = (category) => {
+    const receivedTime = new Date().toUTCString();
+    localStorage.setItem(`${category} responseReceivedTime`, receivedTime);
+}
+
+const getResponseReceivedTime = (category) => {
+    return localStorage.getItem(`${category} responseReceivedTime`);
+}
+
+const updateProductsInLocalStorage = (category, products) => {
+    localStorage.setItem(`${category} products`, JSON.stringify(products));
+}
+
+const getTimeDifference = (category) => {
+    const currTime = Date.now(); 
+    const lastResponseReceivedTimeStr = getResponseReceivedTime(category); 
+    const lastResponseReceivedTime = lastResponseReceivedTimeStr ? Date.parse(lastResponseReceivedTimeStr) : null;
+    if (lastResponseReceivedTime) {
+        return currTime - lastResponseReceivedTime; 
+    }
+
+    return 10000;
+}
